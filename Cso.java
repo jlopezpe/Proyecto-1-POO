c20177f776755726d5b8d06604caf378286f4e54
jmunozhe <55594489+jmunozhe@users.noreply.github.com>
2020-03-01 16:22:52 -0500

    Add files via upload

--- Cso.java
+++ Cso.java
@@ -1,5 +1,20 @@
 package colorBox;
-
-public class Cso {
-
+public class Cso extends Empleado{
+	public int ganancias(){
+		return Empresa.capital;
+	}
+	public void asignarSueldo(Empleado emp) {
+		if(Empresa.capital>20000000) {
+			emp.sueldo=960000;
+		}
+		else if (Empresa.capital<20000000) {
+			despedir();
+			emp.sueldo=700000;
+		}
+	}
+	public void despedir() {
+		int x=(int) ((Math.random() * ((Empleado.idSiguiente - 2) + 1)) + 2);
+		Empleado y=Empleado.getEmpleado(x);
+		y=null;
+	}
 }
