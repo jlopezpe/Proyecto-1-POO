caaf9c5088df054b9bca8f40dc3e086969e86949
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-01 17:21:32 -0500

    Create Repartidor.java

---
+++ Repartidor.java
@@ -0,0 +1,36 @@
+public class Repartidor extends Empleado{
+    public  int numRepartidor=2;
+    public static int idRepar;
+    public String placa;
+    public static int max=5;
+    ArrayList<Dulce> pedido= new ArrayList<>();
+    int numPedidos=pedido.size();
+    
+    Repartidor(String nombre,String plac,String cargo){
+        numRepartidor=idRepar;
+        this.nombre=nombre;
+        this.cargo=cargo;
+        placa=plac;
+        idRepar++;
+    }
+    public void pedido(Dulce algo){
+        if(this.disponibilidad(idRepar)==true){
+            pedido.add(algo); 
+        }
+        else{
+            System.out.println("no hay Repartidores disponibles, por favo intentelo más tarde");
+        }
+               
+    }
+    public boolean disponibilidad(int repa1){
+        for (int i=0;i<numPedidos;i++){
+            if (i<max){
+                return true;
+            }
+            else{
+                return(false);
+            }
+        }
+        return false;
+    }
+}
