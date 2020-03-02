e4bde088ce4da7ee6ddebb90521428647c313d2a
JuanJoUN <55594912+JuanJoUN@users.noreply.github.com>
2020-03-01 22:14:05 -0500

    Update Cso.java

--- Cso.java
+++ Cso.java
@@ -1,20 +1,22 @@
-package colorBox;
-public class Cso extends Empleado{
-	public int ganancias(){
-		return Empresa.capital;
-	}
-	public void asignarSueldo(Empleado emp) {
-		if(Empresa.capital>20000000) {
-			emp.sueldo=960000;
-		}
-		else if (Empresa.capital<20000000) {
-			despedir();
-			emp.sueldo=700000;
-		}
-	}
-	public void despedir() {
-		int x=(int) ((Math.random() * ((Empleado.idSiguiente - 2) + 1)) + 2);
-		Empleado.getEmpleado(x)=null;
-		//y=null;
-	}
+package Test;
+import java.util.*;
+public class Cso extends Empleado {
+
+    public int ganancias() {
+        return Empresa.capital;
+    }
+
+    public void asignarSueldo(Empleado emp) {
+        if (Empresa.capital > 20000000) {
+            emp.sueldo = 960000;
+        } else if (Empresa.capital < 20000000) {
+            despedir();
+            emp.sueldo = 700000;
+        }
+    }
+    
+    public void despedir(){
+        int x=(int) ((Math.random() * ((Empleado.idSiguiente - 2) + 1)) + 2);
+        Empleado.listaEmpleados.remove(x);
+    }
 }
