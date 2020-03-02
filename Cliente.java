43cb5803c556c0022de95f4912a9bf2deea6997a
jlopezpe <55594496+jlopezpe@users.noreply.github.com>
2020-03-01 21:22:50 -0500

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,18 @@
+
+public class Cliente extends Persona{
+	
+	long cel;
+	String Direccion;
+	
+	public void hacerPedido() {
+		
+		
+		
+		//ejecutar metodo de secretaria tomrarpedido()
+		
+		
+		
+		
+	}
+
+}
