bf575650c42d34b2f34024de40a1fdbc97dfc621
JuanJoUN <55594912+JuanJoUN@users.noreply.github.com>
2020-03-02 21:34:59 -0500

    Add files via upload

---
+++ Factura.java
@@ -0,0 +1,23 @@
+
+package Test;
+
+
+public class Factura {
+    String tipo=Manufacturera.tipo_dulce;
+    
+    public String codigo;
+    public int generarCosto(String tipo){
+        int precio=0;
+        if(tipo.equals("chocolate")){
+            precio+=2000*Dulce.cantidad;
+        }else if(tipo.equals("gomitas")){
+            precio+=300*Dulce.cantidad;
+        }else if(tipo.equals("confites")){
+            precio+=200*Dulce.cantidad;
+        }
+        return precio;
+    }
+    public String generarFactura(){
+        
+    }
+}
