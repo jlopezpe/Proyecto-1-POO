72f7d8ae55b5f051ef8fc03722c9a895c53569e2
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2020-03-04 16:32:50 -0500

    Update TomarPedido.java

--- TomarPedido.java
+++ TomarPedido.java
@@ -1,7 +1,23 @@
 package colorBox;
-
-public class TomarPedido extends OpcionDeMenu{
-	public void ejecutar() {
-		Secretaria.tomarPedido();
+import java.util.*;
+public class hacerPedido {
+	Scanner input = new Scanner(System.in);
+	public  void ejecutar() {
+		System.out.println("Bienvenido(a), para hacer su pedido por favor elija el tipo de dulce");
+		System.out.println("- Chocolate");
+        System.out.println("- Gomitas");
+        System.out.println("- Confites");
+        String tipo_dulce = input.next();
+        System.out.println("Digite la cantidad de "+tipo_dulce+"que desea");
+        int cantidad_dulce=input.nextInt();
+        Cliente.hacerPedido(tipo_dulce,cantidad_dulce);
+        System.out.println("¿Desea ordenar algo más?");
+        String decision=input.next();
+        if(decision.equals("si")) {
+        	ejecutar();
+        }
+        else {
+        	System.out.println("Gracias por ordenar, su pedido va en camino");
+        }
 	}
 }
