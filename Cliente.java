26eae4bb0044f4f51bfd20d9bc320479ae7ab923
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2020-03-05 18:25:02 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,18 +1,22 @@
 
+import java.util.*;
 public class Cliente extends Persona{
 	
 	long cel;
 	String Direccion;
-	
-	public void hacerPedido(String tipo_dulce,int cantidad_dulce) {
-		
-		
-		
-		//ejecutar metodo de secretaria tomrarpedido()
-		
-		
-		
-		
+	Cliente(String nombre, long cel, String direccion){
+		super(nombre);
+		this.cel=cel;
+		Direccion=direccion;
 	}
-
+	public ArrayList<Factura> miFactura=new ArrayList<>();
+	public void hacerPedido(String tipo_dulce,int cantidad_dulce) {
+		Secretaria.tomarPedido(tipo_dulce, cantidad_dulce);
+	} 
+	public void mostrarFactura() {
+		for(int i=0;i<miFactura.size();i++) {
+			System.out.println(miFactura.get(i));
+		}
+	}
 }
+
