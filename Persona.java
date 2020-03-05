1d6b7f20ad284aace6cd2adf45a3cef7f5915555
JuanJoUN <55594912+JuanJoUN@users.noreply.github.com>
2020-03-05 18:45:11 -0500

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,17 @@
+
+package Test;
+
+
+public class Persona {
+    public String nombre;
+    public long id;
+    Persona(){}
+    Persona(String nombre){
+        this.id=id;
+        this.nombre=nombre;
+    }
+    Persona(long id,String nombre){
+        this.nombre=nombre;
+        this.id=id;
+    }
+}
