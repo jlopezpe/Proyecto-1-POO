115aa276745cc8bcd89462331772c536e2894333
jlopezpe <55594496+jlopezpe@users.noreply.github.com>
2020-03-05 13:50:35 -0500

    Update TomarPedido.java

--- TomarPedido.java
+++ TomarPedido.java
@@ -5,19 +5,19 @@ public class hacerPedido extends OpcioDeMenu {
 	public  void ejecutar() {
 		System.out.println("Bienvenido(a), para hacer su pedido por favor elija el tipo de dulce");
 		System.out.println("- Chocolate");
-        System.out.println("- Gomitas");
-        System.out.println("- Confites");
-        String tipo_dulce = input.next();
-        System.out.println("Digite la cantidad de "+tipo_dulce+"que desea");
-        int cantidad_dulce=input.nextInt();
-        Cliente.hacerPedido(tipo_dulce,cantidad_dulce);
-        System.out.println("¿Desea ordenar algo más?");
-        String decision=input.next();
-        if(decision.equals("si")) {
-        	ejecutar();
-        }
-        else {
-        	System.out.println("Gracias por ordenar, su pedido va en camino");
-        }
+        	System.out.println("- Gomitas");
+        	System.out.println("- Confites");
+       	 	String tipo_dulce = input.next();
+        	System.out.println("Digite la cantidad de "+tipo_dulce+"que desea");
+        	int cantidad_dulce=input.nextInt();
+        	Cliente.hacerPedido(tipo_dulce,cantidad_dulce);
+        	System.out.println("¿Desea ordenar algo más?");
+        	String decision=input.next();
+        	if(decision.equals("si")) {
+        		ejecutar();
+       		}
+        	else {
+        		System.out.println("Gracias por ordenar, su pedido va en camino");
+        	}
 	}
 }
