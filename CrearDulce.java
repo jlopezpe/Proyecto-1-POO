577313bc638150e91cd938fbec5c1c167a81dafe
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-05 18:56:51 -0500

    Update CrearDulce.java

--- CrearDulce.java
+++ CrearDulce.java
@@ -1,8 +1,14 @@
 public class CrearDulce extends OpcionDeMenu{
   public void ejecutar(){ 
+    if (Secretaria.tipo.equals("chocolate"){
+    System.out.println("Usted acaba de crear 80 unidades de chocolate);
+    }
+     if (Secretaria.tipo.equals("gomitas"){
     System.out.println("Usted acaba de crear 80 unidades de "+ Secretaria.tipo);
-    System.out.println("Usted acaba de crear 80 unidades de "+ Secretaria.tipo);
+     }
+     if (Secretaria.tipo.equals("caramelo"){
     System.out.println("Usted acaba de crear 80 unidades de " + Secretaria.tipo);
+     }
     System.out.println("Aún quedan en el almacenamiento" + Manufactura.Azucar +" kilos de azucar" + Manufactura.Colorante + " litros de colorante"+ Manufactura.Almidon +" kilos de almidón"); 
   } 
 }
