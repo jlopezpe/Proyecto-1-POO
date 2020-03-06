9fd6331f145b72f99c17675c71de9344cd88ead7
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-06 10:26:18 -0500

    Add files via upload

---
+++ GestorAplicacion/Dulce.java
@@ -0,0 +1,9 @@
+public class Dulce {
+	public String nombre;
+	
+	public Dulce(String nombre) {
+		
+		this.nombre=nombre;
+	}
+
+}
---
+++ GestorAplicacion/Empresa.java
@@ -0,0 +1,30 @@
+public class Empresa {
+    public int NIT;
+    public int RUT;
+    public String nombre;
+    public String direccion;
+    public static int capital=40000000;
+    public static int costos=0;
+    public static int ventas=0;
+    public Empresa(int NIT,int RUT,String nombre,String direccion){
+        this.NIT=NIT;
+        this.RUT=RUT;
+        this.direccion=direccion;
+        this.nombre=nombre;
+    }
+
+    public static void getEmpleados(){
+
+        for(Empleado n:Empleado.listaEmpleados){
+            System.out.println(n.getId() + "     " + n.nombre + "     " + n.cargo);
+        }
+
+    }
+
+    public void crearEmpleado(String nom,String carg){
+
+        Empleado.listaEmpleados.add(new Empleado(nom,carg));
+    }
+
+
+}
