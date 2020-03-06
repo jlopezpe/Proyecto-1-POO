255b87a09164dd4c60834c490d704eaa16d5d72f
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-05 20:26:04 -0500

    Update ManufacturaF.java

--- ManufacturaF.java
+++ ManufacturaF.java
@@ -1,35 +1,91 @@
 import java.util.*;
-public class ManufacturaF {
-	
-	static int Azucar=600;
-	static int Colorante=600;
-	static int Almidon=600;
-	
-	public static ArrayList<Dulce> Deposito_caramelos= new ArrayList<Dulce>();
-	public static ArrayList<Dulce> Deposito_chocolate= new ArrayList<Dulce>();
-	public static ArrayList<Dulce> Deposito_gomitas= new ArrayList<Dulce>();
-  
-	public static void crearDulce(String tipo) {
-		
-		if(Azucar<100 || Almidon<100 || Colorante<100) {
-			Secretaria.llamarProveedor();
-			if(tipo.equals("chocolate") {
-				Deposito_chocolate.add(new Dulce("chocolate"));
-				Azucar=-300;
-				Almidon=-200;
+
+public class Manufactura {
+
+    public static int Azucar = 600;
+    public static int Colorante = 600;
+    public static int Almidon = 600;
+
+    public static ArrayList<Dulce> Deposito_caramelos = new ArrayList<Dulce>();
+    public static ArrayList<Dulce> Deposito_chocolate = new ArrayList<Dulce>();
+    public static ArrayList<Dulce> Deposito_gomitas = new ArrayList<Dulce>();
+
+    public static void CrearDulce(String tipo) {
+
+        if (Azucar < 30 || Almidon < 30 || Colorante < 30) {
+            Secretaria.llamarProveedor();
+            if (tipo.equals("chocolate")) {
+                for (int i = 0; i < 50; i++) {
+                    Deposito_chocolate.add(new Dulce("chocolate"));
+                    Azucar = -6;
+                    Almidon = -4;
+                }
+            }
+            if (tipo.equals("caramelo")) {
+                for (int i = 0; i < 50; i++) {
+                    Deposito_caramelos.add(new Dulce("caramelo"));
+                    Colorante = -5;
+                    Azucar = -7;
+                    Almidon = -3;
+                }
+            }
+            if (tipo.equals("gomitas")) {
+                for (int i = 0; i < 50; i++) {
+                    Deposito_gomitas.add(new Dulce("gomitas"));
+                    Colorante = -4;
+                    Azucar = -7;
+                    Almidon = -3;
+                }
+            }
+
+        } else {
+            if (tipo.equals("chocolate")) {
+                for (int i = 0; i < 50; i++) {
+                    if (Azucar > 6 && Almidon > 4) {
+                        Deposito_chocolate.add(new Dulce("chocolate"));
+                        Azucar = -6;
+                        Almidon = -4;
+                    } else {
+                        Secretaria.llamarProveedor();
+                        Deposito_chocolate.add(new Dulce("chocolate"));
+                        Azucar = -6;
+                        Almidon = -4;
+                    }
+                }
+            }
+            if (tipo.equals("caramelo")) {
+                for (int i = 0; i < 50; i++) {
+                    if (Colorante > 5 && Azucar > 7 && Almidon > 3) {
+                        Deposito_caramelos.add(new Dulce("caramelo"));
+                        Colorante = -5;
+                        Azucar = -7;
+                        Almidon = -3;
+                    } 
+                    else {
+                        Secretaria.llamarProveedor();
+                        Deposito_caramelos.add(new Dulce("caramelo"));
+                        Colorante = -5;
+                        Azucar = -7;
+                        Almidon = -3;
+                    }
+                }
+            }
+            if (tipo.equals("gomitas")) {
+                for (int i = 0; i < 50; i++) {
+                    if (Colorante > 4 && Azucar > 7 && Almidon > 3) {
+                        Deposito_gomitas.add(new Dulce("gomitas"));
+                        Colorante = -4;
+                        Azucar = -7;
+                        Almidon = -3;
+                    } else {
+                        Secretaria.llamarProveedor();
+                        Deposito_gomitas.add(new Dulce("gomitas"));
+                        Colorante = -4;
+                        Azucar = -7;
+                        Almidon = -3;
+                    }
+                }
+            }
         }
-        if(tipo.equals("caramelo") {
-				Deposito_caramelos.add(new Dulce("caramelo"));
-				Colorante=-250;
-				Azucar=-350;
-				Almidon=-150;
-        }
-        if(tipo.equals("gomitas") {
-				Deposito_gomitas.add(new Dulce("gomitas"));
-				Colorante=-200;
-				Azucar=-350;
-				Almidon=-150;
-        }
-			}		
-		}
+    }
 }
