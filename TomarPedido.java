4f8b497bf0ce9c6859c1ce725aae179f821cf24d
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2020-03-05 19:49:35 -0500

    Update TomarPedido.java

--- TomarPedido.java
+++ TomarPedido.java
@@ -1,19 +1,20 @@
 package colorBox;
+
 import java.util.Scanner;
 
-public class HacerPedido extends OpcionDeMenu {
+public class hacerPedido1  {
 	
 	public  void ejecutar() {
 		
 		Scanner input=new Scanner (System.in);
-		System.out.print("Antes de hacer el pedido, por favor responda las preguntas del formulario.");
-		System.out.print("Cual es su nombre?");
+		System.out.print("Bienvenido(a), antes de hacer el pedido, por favor responda las preguntas del formulario.");
+		System.out.print("¿Cuál es su nombre?");
 		String nombre=input.next();
-		System.out.print("Cual es su direccion?");
+		System.out.print("¿Cuál es su dirección?");
 		String direccion=input.next();
-		System.out.print("Cual es su telefono");
+		System.out.print("¿Cuál es su teléfono");
 		int celular=input.nextInt();
-		new Cliente(nombre,direccion,celular);
+		Cliente cliente =new Cliente(nombre,celular,direccion);
 		
 		
 		
@@ -24,23 +25,23 @@ public class HacerPedido extends OpcionDeMenu {
 		int condicion_while=1;
 		while(condicion_while==1) {
 		
-			System.out.println("Bienvenido(a), para hacer su pedido por favor elija el tipo de dulce");
+			System.out.println("Para hacer su pedido por favor elija el tipo de dulce");
 			System.out.println("- Chocolate");
         		System.out.println("- Gomitas");
         		System.out.println("- Confites");
         		String tipo_dulce = input.next();
         		System.out.println("Digite la cantidad de "+tipo_dulce+"que desea");
         		int cantidad_dulce=input.nextInt();
-        		new Cliente(nombre,direccion,celular).hacerPedido(tipo_dulce, cantidad_dulce);
+        		cliente.hacerPedido(tipo_dulce, cantidad_dulce);
         		System.out.println("¿Desea ordenar algo más?");
         		String decision=input.next();
-        		if(decision.equals("si")) {
-        			ejecutar();
-        		}
-        		else {
-        			System.out.println("Gracias por ordenar, su pedido va en camino");
+        		if(decision.equals("no")) {
+        			System.out.println("Gracias por ordenar, este es su pedido: ");
+        			cliente.mostrarFactura();
+        			System.out.println("El repartidor va en camino, gracias por utilizar nuestros servicios. Hasta luego");
         			condicion_while=0;
         		}
+        		
 		}
 	}
 }
