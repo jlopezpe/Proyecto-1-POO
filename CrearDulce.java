d7af84a8f7efe7c7069b98f0869f7405dda3903f
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-06 00:46:46 -0500

    Update CrearDulce.java

--- CrearDulce.java
+++ CrearDulce.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class CrearDulce extends OpcionDeMenu{
   public void ejecutar(){
     Scanner input=new Scanner (System.in);
-    System.out.println("Qué tipo de dulce la gustaría crear, por favor escriba como se muestran las opciones \n chocolate \n gomitas \n caramelo");
+    System.out.println("QuÃ© tipo de dulce la gustarÃ­a crear, por favor escriba como se muestran las opciones \n chocolate \n gomitas \n caramelo");
     String tipo =input.next();
-    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa hará 50 ejemplares del tipo de dulce que haya escogido por defecto de la máquina");
+    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa harÃ¡ 50 ejemplares del tipo de dulce que haya escogido por defecto de la mÃ¡quina");
     if (tipo.equals("chocolate")){
       Manufactura.CrearDulce(tipo);
       System.out.println("Se han creado 50 unidades de chocolate");
@@ -18,6 +18,5 @@ public class CrearDulce extends OpcionDeMenu{
       Manufactura.CrearDulce(tipo);
       System.out.println("Se han creado 50 unidades de caramelos");
     }
-    System.out.println("Aún quedan en el almacenamiento" + Manufactura.Azucar +" kilos de azucar" + Manufactura.Colorante + " litros de colorante"+ Manufactura.Almidon +" kilos de almidón"); 
   } 
 }
