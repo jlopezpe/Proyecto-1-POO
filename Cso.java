50fa240bcf8c81c1aff71a517a6a89d52a70a5e3
jmunozhe <55594489+jmunozhe@users.noreply.github.com>
2020-03-05 19:01:40 -0500

    Update Cso.java

--- Cso.java
+++ Cso.java
@@ -19,4 +19,36 @@ public class Cso extends Empleado {
         int x=(int) ((Math.random() * ((Empleado.idSiguiente - 2) + 1)) + 2);
         Empleado.listaEmpleados.remove(x);
     }
+      public String repartidorMes(Repartidor a,Repartidor b) {
+	  if(a.puntosTrabajo==b.puntosTrabajo) {
+		  return "Los dos han trabajado por igual,nadie recibe aumento";
+	  }
+	  else if(a.puntosTrabajo>b.puntosTrabajo) {
+		  if(checkUp(a)) {
+			  a.sueldo+=100000;
+			  return "El repartidor: "+a.nombre+"ha recibido un aumento";
+		  }
+		  else {
+			  return "No se ha podido hacer el aumento,el capital va en bajada";
+		  }
+	  }
+	  else if(b.puntosTrabajo>a.puntosTrabajo) {
+		  if(checkUp(b)) {
+			  b.sueldo+=100000;
+			  return "El repartidor: "+b.nombre+"ha recibido un aumento";
+		  }
+		  else {
+			  return "No se ha podido hacer el aumento,el capital va en bajada";
+		  }
+	  }
+	  return "fin";
+  }
+  public boolean checkUp(Repartidor a) {
+	  if(Empresa.capital>15000000) {
+		  return true;
+	  }
+	  else {
+		  return false;
+	  }
+  }
 }
