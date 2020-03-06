0c54531b8c657479de31119e10231242d28ad50a
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-05 22:32:18 -0500

    Update CrearDulce.java

--- CrearDulce.java
+++ CrearDulce.java
@@ -4,17 +4,17 @@ public class CrearDulce extends OpcionDeMenu{
     System.out.println("Qué tipo de dulce la gustaría crear, por favor escriba como se muestran las opciones "\n"chocolate" \n "gomitas" \n "caramelo");
     String tipo =input.next();
     System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa hará 50 ejemplares del tipo de dulce que haya escogido por defecto de la máquina");
-    if (tipo.equals("chocolate"){
-      Manufactura.crearDulce(tipo)
-      System.out.println("Se han creado 50 unidades de chocolate);
+    if (tipo.equals("chocolate")){
+      Manufactura.crearDulce(tipo);
+      System.out.println("Se han creado 50 unidades de chocolate");
     }
-    if (tipo.equals("gomitas"){
-      Manufactura.crearDulce(tipo)
-      System.out.println("Se han creado 50 unidades de gomitas);
+    if (tipo.equals("gomitas")){
+      Manufactura.crearDulce(tipo);
+      System.out.println("Se han creado 50 unidades de gomitas");
     }
-     if (tipo.equals("caramelo"){
-      Manufactura.crearDulce(tipo)
-      System.out.println("Se han creado 50 unidades de caramelos);
+     if (tipo.equals("caramelo")){
+      Manufactura.crearDulce(tipo);
+      System.out.println("Se han creado 50 unidades de caramelos");
     }
     System.out.println("Aún quedan en el almacenamiento" + Manufactura.Azucar +" kilos de azucar" + Manufactura.Colorante + " litros de colorante"+ Manufactura.Almidon +" kilos de almidón"); 
   } 
