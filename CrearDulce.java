521564afd4fb7e1256d5b3a3fb284410f7b54aa9
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-05 20:26:53 -0500

    Update CrearDulce.java

--- CrearDulce.java
+++ CrearDulce.java
@@ -3,7 +3,7 @@ public class CrearDulce extends OpcionDeMenu{
     Scanner input=new Scanner (System.in);
     System.out.println("Qué tipo de dulce la gustaría crear, por favor escriba como se muestran las opciones "\n"chocolate" \n "gomitas" \n "caramelo");
     String tipo =input.next();
-    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa hará 50 ejemplares del tipo de dulce que haya escogido");
+    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa hará 50 ejemplares del tipo de dulce que haya escogido por defecto de la máquina");
     if (tipo.equals("chocolate"){
       ManufacturaF.crearDulce(tipo)
       System.out.println("Usted acaba de crear 50 unidades de chocolate);
