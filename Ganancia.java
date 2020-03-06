c5a4d27d58171b3f7b420bc6d4f518ace3042564
jmunozhe <55594489+jmunozhe@users.noreply.github.com>
2020-03-05 22:53:45 -0500

    Update Ganancia.java

--- Ganancia.java
+++ Ganancia.java
@@ -1,12 +1,11 @@
 public class Ganancia extends OpcionDeMenu{
-  public void ejecutar(){
-    
-    cso.ganancias();
-    if(cso.ganancias() >=0){
-    System.out.println("Esta es la ganancia de la empresa" + cso.ganancias() +"pesos");
-    }
-    else{
-      System.out.println("Lamentablemente la empresa ha entrado en bancarrota con una perdida de " + (cso.ganancias())*-1 +"pesos");
-  } 
-}
-}
+	  public void ejecutar(){	    
+	    Cso.ganancias();
+	    if(Cso.ganancias() >=0){
+	    System.out.println("Esta es la ganancia de la empresa" + Cso.ganancias() +"pesos");
+	    }
+	    else{
+	      System.out.println("Lamentablemente la empresa ha entrado en bancarrota con una perdida de " + (Cso.ganancias())*-1 +" pesos");
+	  } 
+	}
+	}
