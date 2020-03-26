3a00eea0756c4c7b55e34265a0b2750bee4c40c5
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2020-03-26 16:56:43 -0500

    Update Cso.java

--- Personas/Cso.java
+++ Personas/Cso.java
@@ -3,83 +3,118 @@ package Personas;
 
 import Procesos.Empresa;
 public class Cso extends Empleado {
-	public static int ventaChocolate;
-	public static int ventaGomitas;
-	public static int ventaCaramelos;
-	public Cso(String nombre,int sueldo){
+    private static int ventaChocolate;
+    private static int ventaGomitas;
+    private static int ventaCaramelos;
+    public Cso(String nombre,int sueldo){
         super(nombre,sueldo);
     }
     public static int ganancias() {
-    	
-    	int resultado=Empresa.ventas-Empresa.costos;
-    	
-    	if(resultado<0) {
-    		return resultado;
-    	}
-    	else {
-    		return Empresa.ventas-Empresa.costos;
-    	}
-    	
+
+        int resultado=Empresa.getVentas()-Empresa.getCostos();
+
+        if(resultado<0) {
+            return resultado;
+        }
+        else {
+            return Empresa.getVentas()-Empresa.getCostos();
+        }
+
     }
 
     public static void asignarSueldo(Empleado emp) {
-        if (Empresa.capital > 20000000) {
-            emp.sueldo = 960000;
-        } else if (Empresa.capital < 20000000) {
-            despedir();
-            emp.sueldo = 700000;
+        if (Empresa.getCapital() > 20000000) {
+            emp.setSueldo(960000);
+        } else if (Empresa.getCapital() < 20000000) {
+            emp.setSueldo(700000);
         }
     }
-    
-    public static void despedir(){
-        int x=(int) ((Math.random() * ((Empleado.idSiguiente - 2) + 1)) + 2);
-        Empleado.listaEmpleados.remove(x);
+
+   
+    public static String repartidorMes() {
+        Repartidor a=(Repartidor) Empleado.getEmpleado(3);
+        Repartidor b=(Repartidor) Empleado.getEmpleado(4);
+        if(a.puntosTrabajo==b.puntosTrabajo) {
+            return "Los dos han trabajado por igual,nadie recibe aumento";
+        }
+        else if(a.puntosTrabajo>b.puntosTrabajo) {
+            if(checkUp()) {
+                a.setSueldo(a.getSueldo()+100000);
+                return "El repartidor: "+a.getNombre()+"ha recibido un aumento";
+            }
+            else {
+                return "No se ha podido hacer el aumento,el capital va en bajada";
+            }
+        }
+        else if(b.puntosTrabajo>a.puntosTrabajo) {
+            if(checkUp()) {
+                b.setSueldo(b.getSueldo()+100000);
+                return "El repartidor: "+b.getNombre()+"ha recibido un aumento";
+            }
+            else {
+                return "No se ha podido hacer el aumento,el capital va en bajada";
+            }
+        }
+        return "fin";
     }
-     public static String repartidorMes() {
-	 Repartidor a=(Repartidor) Empleado.getEmpleado(3);
-	 Repartidor b=(Repartidor) Empleado.getEmpleado(4);
-	  if(a.puntosTrabajo==b.puntosTrabajo) {
-		  return "Los dos han trabajado por igual,nadie recibe aumento";
-	  }
-	  else if(a.puntosTrabajo>b.puntosTrabajo) {
-		  if(checkUp()) {
-			  a.sueldo+=100000;
-			  return "El repartidor: "+a.nombre+"ha recibido un aumento";
-		  }
-		  else {
-			  return "No se ha podido hacer el aumento,el capital va en bajada";
-		  }
-	  }
-	  else if(b.puntosTrabajo>a.puntosTrabajo) {
-		  if(checkUp()) {
-			  b.sueldo+=100000;
-			  return "El repartidor: "+b.nombre+"ha recibido un aumento";
-		  }
-		  else {
-			  return "No se ha podido hacer el aumento,el capital va en bajada";
-		  }
-	  }
-	  return "fin";
-  }
-  public static boolean checkUp() {
-	  if(Empresa.capital>15000000) {
-		  return true;
-	  }
-	  else {
-		  return false;
-	  }
-  }
-	 public static String mayorVenta() {
-	  String mensaje="";
-	  if(ventaChocolate>ventaGomitas && ventaChocolate>ventaCaramelos) {
-		  mensaje="Chocolate con "+ventaChocolate+" unidades";
-	  }
-	  else if(ventaGomitas>ventaChocolate && ventaGomitas>ventaCaramelos) {
-		  mensaje="Gomitas con "+ventaGomitas+" unidades";
-	  }
-	  else if(ventaCaramelos>ventaChocolate && ventaCaramelos>ventaGomitas) {
-		  mensaje="Caramelos con "+ventaCaramelos+" unidades";
-	  }
-		 return mensaje;
-  }
+    public static boolean checkUp() {
+        if(Empresa.getCapital()>15000000) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static String mayorVenta() {
+        String mensaje="";
+        if(getVentaChocolate()>getVentaGomitas() && getVentaChocolate()>getVentaCaramelos()) {
+            mensaje="Chocolate";
+        }
+        else if(getVentaGomitas()>getVentaChocolate() && getVentaGomitas()>getVentaCaramelos()) {
+            mensaje="Gomitas";
+        }
+        else if(getVentaCaramelos()>getVentaChocolate() && getVentaCaramelos()>getVentaGomitas()) {
+            mensaje="Caramelos";
+        }
+        return mensaje;
+    }
+
+    public static String unidades_mayorVenta(){
+        String mensaje2="";
+        if(getVentaChocolate()>getVentaGomitas() && getVentaChocolate()>getVentaCaramelos()) {
+            mensaje2=String.valueOf(getVentaChocolate());
+        }
+        else if(getVentaGomitas()>getVentaChocolate() && getVentaGomitas()>getVentaCaramelos()) {
+            mensaje2=String.valueOf(getVentaGomitas());
+        }
+        else if(getVentaCaramelos()>getVentaChocolate() && getVentaCaramelos()>getVentaGomitas()) {
+            mensaje2=String.valueOf(getVentaCaramelos());
+
+        }
+        return mensaje2;
+    }
+
+    public static int getVentaChocolate() {
+        return ventaChocolate;
+    }
+
+    public static void setVentaChocolate(int ventaChocolate) {
+        Cso.ventaChocolate = ventaChocolate;
+    }
+
+    public static int getVentaGomitas() {
+        return ventaGomitas;
+    }
+
+    public static void setVentaGomitas(int ventaGomitas) {
+        Cso.ventaGomitas = ventaGomitas;
+    }
+
+    public static int getVentaCaramelos() {
+        return ventaCaramelos;
+    }
+
+    public static void setVentaCaramelos(int ventaCaramelos) {
+        Cso.ventaCaramelos = ventaCaramelos;
+    }
 }
