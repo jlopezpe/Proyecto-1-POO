d604cc6e8aea9524aa8ed12f1939405f20392715
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-26 14:12:58 -0500

    Update AsignarSueldo.java

--- UiMain/AsignarSueldo.java
+++ UiMain/AsignarSueldo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import Personas.Cso;
 import Personas.Empleado;
 public class AsignarSueldo extends OpcionDeMenu{
-public void ejecutar(){
+public String ejecutar(){
     Scanner input= new Scanner(System.in);
     System.out.println("Ingrese el nombre del trabajador ");
     String nombre=input.next();
@@ -11,6 +11,17 @@ public void ejecutar(){
     String cargo=input.next();
     Empleado x=new Empleado(nombre,cargo);
     Cso.asignarSueldo(x);
-    System.out.println("El sueldo asignado a "+x.nombre+" fue "+x.sueldo);
+    return("El sueldo asignado a "+x.nombre+" fue "+x.sueldo);
 } 
-}
+public String ejecutar1(String nombre,String cargo){
+	   // Scanner input= new Scanner(System.in);
+	   // System.out.println("Ingrese el nombre del trabajador ");
+	   // String nombre=input.next();
+	   // System.out.println("Ingrese el cargo ");
+	   // String cargo=input.next();
+	    Empleado x=new Empleado(nombre,cargo);
+	    Cso.asignarSueldo(x);
+	    System.out.println("El sueldo asignado a "+x.nombre+" fue "+x.sueldo+" pesos");
+	    return "El sueldo asignado a "+x.nombre+" fue "+x.sueldo+" pesos";
+	}
+	}
