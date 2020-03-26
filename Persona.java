b6d6a0583a92a34205abbdc76fd1241b929bb0fe
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2020-03-26 13:49:52 -0500

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,14 +1,16 @@
 package Personas;
-
 public class Persona {
-    public String nombre;
+    private String nombre;
     private long id;
     Persona(){}
     Persona(String nombre){
         this.nombre=nombre;
     }
     Persona(long id,String nombre){
-        this.nombre=nombre;
+        this(nombre);
         this.id=id;
     }
+	public String getNombre() {
+		return nombre;
+	}
 }
