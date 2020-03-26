cea2147b9fc2b6063c6bb576e62d50b7eaef8b8d
JuanJoUN <55594912+JuanJoUN@users.noreply.github.com>
2020-03-25 20:30:56 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,23 +1,39 @@
 package Personas;
-import java.util.*;
 
 import Procesos.Factura;
+import java.util.*;
 public class Cliente extends Persona{
-	
-	long cel;
-	String Direccion;
-	public Cliente(String nombre, long cel, String direccion){
-		super(nombre);
-		this.cel=cel;
-		Direccion=direccion;
-	}
-	public ArrayList<Factura> miFactura=new ArrayList<>();
-	public void hacerPedido(String tipo_dulce,int cantidad_dulce) {
-		Secretaria.tomarPedido(tipo_dulce, cantidad_dulce,this);
-	} 
-	public void mostrarFactura() {
-		for(int i=0;i<miFactura.size();i++) {
-			System.out.println(miFactura.get(i));
-		}
-	}
+
+    private long cel;
+    private String Direccion;
+    public Cliente(String nombre, long cel, String direccion){
+        super(nombre);
+        this.cel=cel;
+        Direccion=direccion;
+    }
+    public ArrayList<Factura> miFactura=new ArrayList<>();
+    public void hacerPedido(String tipo_dulce,int cantidad_dulce) {
+        Secretaria.tomarPedido(tipo_dulce, cantidad_dulce,this);
+    }
+    public void mostrarFactura() {
+        for(int i=0;i<miFactura.size();i++) {
+            System.out.println(miFactura.get(i));
+        }
+    }
+
+    public long getCel() {
+        return cel;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setCel(long cel) {
+        this.cel = cel;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
 }
