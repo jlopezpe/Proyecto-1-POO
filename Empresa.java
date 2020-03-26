fc2322c8580b8d912316dec715e870620c6d6fb8
jcorreapa <55594799+jcorreapa@users.noreply.github.com>
2020-03-26 13:47:02 -0500

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -1,15 +1,11 @@
-package Procesos;
-
-import Personas.Empleado;
-
 public class Empresa {
     public int NIT;
     public int RUT;
     public String nombre;
     public String direccion;
-    public static int capital=40000000;
-    public static int costos=0;
-    public static int ventas=0;
+    private static int capital=40000000;
+    private static int costos=0;
+    private static int ventas=0;
     public Empresa(int NIT,int RUT,String nombre,String direccion){
         this.NIT=NIT;
         this.RUT=RUT;
@@ -20,7 +16,7 @@ public class Empresa {
     public static void getEmpleados(){
 
         for(Empleado n:Empleado.listaEmpleados){
-            System.out.println(n.getId() + "     " + n.nombre + "     " + n.cargo);
+            System.out.println(n.getId() + "     " + n.getNombre() + "     " + n.getCargo());
         }
 
     }
@@ -29,6 +25,18 @@ public class Empresa {
 
         Empleado.listaEmpleados.add(new Empleado(nom,carg));
     }
+
+	public static int getCostos() {
+		return costos;
+	}
+
+	public static int getVentas() {
+		return ventas;
+	}
+
+	public static int getCapital() {
+		return capital;
+	}
 
 
 }
