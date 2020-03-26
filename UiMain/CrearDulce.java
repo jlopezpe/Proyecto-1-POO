07f83ee9f7897ac1464183bb00ea38e7f3e80b65
mmunozal <55594437+mmunozal@users.noreply.github.com>
2020-03-26 14:11:18 -0500

    Update CrearDulce.java

--- UiMain/CrearDulce.java
+++ UiMain/CrearDulce.java
@@ -1,26 +1,45 @@
-
 package UiMain;
 
 import java.util.*;
 
 import Procesos.Manufactura;
 public class CrearDulce extends OpcionDeMenu{
-  public void ejecutar(){
+  public String ejecutar(){
     Scanner input=new Scanner (System.in);
-    System.out.println("QuÈ tipo de dulce la gustarÌa crear, por favor escriba como se muestran las opciones \n chocolate \n gomitas \n caramelo");
+
     String tipo =input.next();
-    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa har· 50 ejemplares del tipo de dulce que haya escogido por defecto de la m·quina");
+    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa har√° 50 ejemplares del tipo de dulce que haya escogido por defecto de la m√°quina");
     if (tipo.equals("chocolate")){
       Manufactura.CrearDulce(tipo);
-      System.out.println("Se han creado 50 unidades de chocolate");
+      return("Se han creado 50 unidades de chocolate");
     }
     if (tipo.equals("gomitas")){
       Manufactura.CrearDulce(tipo);
-      System.out.println("Se han creado 50 unidades de gomitas");
+      return("Se han creado 50 unidades de gomitas");
     }
      if (tipo.equals("caramelo")){
       Manufactura.CrearDulce(tipo);
-      System.out.println("Se han creado 50 unidades de caramelos");
+      return("Se han creado 50 unidades de caramelos");
     }
-  } 
+	return tipo;
+  }
+  public String ejecutar1(String tip){
+	    System.out.println("Estamos verificando, espere un momento, recuerde que nuestra empresa har√° 50 ejemplares del tipo de dulce que haya escogido por defecto de la m√°quina");
+	    if (tip.equals("chocolate")){
+	      Manufactura.CrearDulce(tip);
+	      System.out.println("Se han creado 50 unidades de chocolate");
+	      return "Se han creado 50 unidades de chocolate";
+	    }
+	    else if (tip.equals("gomitas")){
+	      Manufactura.CrearDulce(tip);
+	      System.out.println("Se han creado 50 unidades de gomitas");
+	      return "Se han creado 50 unidades de gomitas";
+	    }
+	    else if (tip.equals("caramelo")){
+	      Manufactura.CrearDulce(tip);
+	      System.out.println("Se han creado 50 unidades de caramelos");
+	      return "Se han creado 50 unidades de caramelos";
+	    }
+		return null;
+	  }
 }
